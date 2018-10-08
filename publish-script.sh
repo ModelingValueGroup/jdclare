5deaafea02093d3a2c5f4e80a3bb0becf9232af3
Tom Brus <tombrus@gmail.com>
2018-10-09 00:27:55 +0200

    make publish work

--- publish-script.sh
+++ publish-script.sh
@@ -2,18 +2,11 @@
 set -ue
 
 ###############################################################################
+   GITHUB_URL="https://api.github.com"
         OWNER="$(git remote -v | head -1 | sed 's|.*:\([^/]*\)/\([^.]*\).*|\1|')"
         REPOS="$(git remote -v | head -1 | sed 's|.*:\([^/]*\)/\([^.]*\).*|\2|')"
-   GITHUB_URL="https://api.github.com"
     REPOS_URL="$GITHUB_URL/repos/$OWNER/$REPOS"
 ###############################################################################
-jsonSelect() {
-    local field="$1"; shift
-
-    tr ',' '\n' \
-        | fgrep '"'"$field"'":' \
-        | sed 's/.*"'"$field"'": *"\([^"{]*\).*/\1/'
-}
 contains() {
     local find="$1"; shift
     local  str="$1"; shift
@@ -39,66 +32,77 @@ curl_() {
 validateToken() {
     local token="$1"; shift
 
-    ! curl_ "$token" "$REPOS_URL" >/dev/null
+    curl_ "$token" "$REPOS_URL" >/dev/null
 }
-releaseExists() {
+getRelease() {
     local token="$1"; shift
     local   tag="$1"; shift
 
-    ! curl_ "$token" "$REPOS_URL/releases/tags/$tag" | fgrep -q '"message": "Not Found"'
+    curl_ "$token" "$REPOS_URL/releases/tags/$tag"
 }
 publishTag() {
+    local     tag="$1"; shift
     local   token="$1"; shift
     local isDraft="$1"; shift
     local  branch="$1"; shift
-    local     tag="$1"; shift
     local  assets=("$@")
 
-    local comment="$(git tag -l -n1 "$tag" | sed 's/[^ ]* //')"
+    local comment="release '$tag' created on $(date +'%Y-%m-%d %H:%M:%S')"
     local   isPre="$(contains pre "$tag")"
-    local relName="$(sed 's/-.*//' <<<$tag)"
 
-    echo "TAG found:"
+    echo "release info:"
     echo "      tag     = $tag"
-    echo "      relName = $relName"
+    echo "      relName = $tag"
     echo "      isDraft = $isDraft"
     echo "      isPre   = $isPre"
     echo "      branch  = $branch"
     echo "      comment = $comment"
 
-    if releaseExists "$token" "$tag"; then
-        echo "    release for this tag already exists, no further action needed."
-    else
-        echo "    no release for this tag yet, going to create it..."
-        json="$(cat <<EOF
+
+    local   relJson="$(getRelease "$token" "$tag")"
+    local uploadUrl="$(jq --raw-output '.upload_url' <<<"$relJson")"
+    if [ "$uploadUrl" != "null" ]; then
+        echo "ERROR: this release already exists, delete it first"
+        exit 99
+    fi
+    echo "    creating new release..."
+    json="$(cat <<EOF
 {
-  "tag_name"        : "$tag",
-  "target_commitish": "$branch",
-  "name"            : "$relName",
-  "body"            : "$comment",
-  "draft"           : $isDraft,
-  "prerelease"      : $isPre
+"tag_name"        : "$tag",
+"target_commitish": "$branch",
+"name"            : "$tag",
+"body"            : "$comment",
+"draft"           : $isDraft,
+"prerelease"      : $isPre
 }
 EOF
 )"
-        local uploadUrl="$(curl_ "$token" -X POST -d "$json" "$REPOS_URL/releases" | jsonSelect 'upload_url')"
-        echo "    using upload url: $uploadUrl"
-        for file in "${assets[@]}"; do
-            if [ ! -f "$file" ]; then
-                echo "ERROR: can not find file $file"
-            else
-                local mimeType="$(file -b --mime-type "$file")"
-                local     name="$(basename "$file" | sed "s/\(.*\)\.\([^.]*\)/\1.$tag.\2/")"
-                echo "      attaching: $file as $name ($mimeType)"
-                local state="$(curl_ "$token" --header "Content-Type: $mimeType" -X POST -d @"$file" "$uploadUrl?name=$name" | jsonSelect 'state')"
-                if [ "$state" == "uploaded" ]; then
-                    echo "        => ok"
-                else
-                    echo "        => bad ($state)"
-                fi
-            fi
-        done
+    local relJson="$(curl_ "$token" -X POST -d "$json" "$REPOS_URL/releases")"
+    local uploadUrl="$(jq --raw-output '.upload_url' <<<"$relJson")"
+    if [ "$uploadUrl" == "null" ]; then
+        echo "ERROR: unable to create the release: $relJson"
+        exit 99
     fi
+    local uploadUrl="$(sed 's/{\?.*//' <<<$uploadUrl)"
+    echo "    using upload url: $uploadUrl"
+
+    for file in "${assets[@]}"; do
+        local mimeType="$(file -b --mime-type "$file")"
+        local     name="$(basename "$file" | sed "s/\(.*\)\.\([^.]*\)/\1.$tag.\2/")"
+        echo "      attaching: $file as $name ($mimeType)"
+        local  attJson="$(curl_ "$token" --header "Content-Type: $mimeType" -X POST -d @"$file" "$uploadUrl?name=$name")"
+        local    state="$(jq --raw-output '.state' <<<"$attJson")"
+        if [ "$state" == "uploaded" ]; then
+            echo "        => ok"
+        else
+            echo "        => bad ($state)"
+            echo "====="
+            echo "$attJson"
+            echo "====="
+            echo "$uploadUrl?name=$name"
+            echo "====="
+        fi
+    done
 }
 currentBranch() {
     if [ "${GIT_BRANCH:-}" != "" ]; then
@@ -108,21 +112,31 @@ currentBranch() {
     fi
 }
 publish() {
-    local  token="$1"; shift
-    local  draft="$1"; shift
-    local assets=("$@")
+    local version="$1"; shift
+    local   token="$1"; shift
+    local isDraft="$1"; shift
+    local  assets=("$@")
+
+    if [ "$version" == "" ]; then
+        echo "ERROR: version is empty"
+        exit 60
+    fi
+    if [ "$(git tag -l "$version")" ]; then
+        echo "ERROR: tag $version already exists"
+        exit 70
+    fi
+    if ! validateToken "$token"; then
+        echo "ERROR: not a valid token"
+        exit 80
+    fi
+    for file in "${assets[@]}"; do
+        if [ ! -f "$file" ]; then
+            echo "ERROR: can not find file $file"
+            exit 90
+        fi
+    done
 
     local branch="$(currentBranch)"
-    if [ "$branch" != master ]; then
-        echo "INFO: not on master branch (but on $branch), so no publishing"
-    elif ! validateToken "$token"; then
-        echo "ERROR: not a valid token"
-    else
-        git tag -l --points-at HEAD \
-            | egrep '^v[0-9]' \
-            | while read tag; do
-                publishTag "$token" "$draft" "$branch" "$tag" "${assets[@]}"
-            done
-    fi
+    publishTag "$version" "$token" "$isDraft" "$branch" "${assets[@]}"
 }
 ###############################################################################
--- publish.sh
+++ publish.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 set -ue
+
+version="$1"; shift
+  token="$1"; shift
+isDraft="$1"; shift
+
 . publish-script.sh
 
-publish "$1" false \
-    out/artifacts/dclare-collections.jar \
-    out/artifacts/dclare-collections-src.jar \
-    out/artifacts/dclare-transactions.jar \
-    out/artifacts/dclare-transactions-src.jar
+if [ "$version" != "" ]; then
+    publish \
+        "$version" \
+        "$token" \
+        "$isDraft" \
+        out/artifacts/dclare-collections.jar \
+        out/artifacts/dclare-collections-src.jar \
+        out/artifacts/dclare-transactions.jar \
+        out/artifacts/dclare-transactions-src.jar
+fi
