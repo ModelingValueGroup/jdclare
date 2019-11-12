8ef637c7adeca826e40aebb04c61eeab789906a2
Tom Brus <tombrus@gmail.com>
2019-11-12 16:00:39 +0100

    remove travis and restructure build

--- build/05-test.sh
+++ build/05-test.sh
@@ -2,32 +2,30 @@
 set -ue
 . build/tmp/prep.sh
 ################################################################
-if [ "$runTests" == true ]; then
-  echo "...testing"
-  generateAntTestFile "mvg-jdclare" > test.xml
+echo "...testing"
+generateAntTestFile "mvg-jdclare" > test.xml
 
-  # to keep travis-ci happy:
-  SECONDS=0
-  while sleep 60; do
-    echo "=====[ still running after $SECONDS sec... ]====="
-  done &
+# to keep travis-ci happy:
+SECONDS=0
+while sleep 60; do
+  echo "=====[ still running after $SECONDS sec... ]====="
+done &
 
 
-  if ! ant -Dpath.variable.maven_repository=$mavenReposDir -f test.xml; then
-    kill %1
-    echo "======================================================================"
-    echo " FAILURES DETECTED"
-    echo "======================================================================"
-    for f in $(\
-          egrep '(errors|failures)="' TEST-* \
-                | egrep -v 'errors="0" failures="0"' \
-                | sed 's/:.*//'
-          ); do
-          [[ -f "$f" ]] && cat $f
-    done
-    echo "======================================================================"
-    exit 99
-  fi
+if ! ant -Dpath.variable.maven_repository=$mavenReposDir -f test.xml; then
   kill %1
-  echo "...all tests ok!"
+  echo "======================================================================"
+  echo " FAILURES DETECTED"
+  echo "======================================================================"
+  for f in $(\
+        egrep '(errors|failures)="' TEST-* \
+              | egrep -v 'errors="0" failures="0"' \
+              | sed 's/:.*//'
+        ); do
+        [[ -f "$f" ]] && cat $f
+  done
+  echo "======================================================================"
+  exit 99
 fi
+kill %1
+echo "...all tests ok!"
