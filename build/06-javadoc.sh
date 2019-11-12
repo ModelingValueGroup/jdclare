4bfad609de7c7455607c72b7722ab956c156f2b7
Tom Brus <tombrus@gmail.com>
2019-11-12 17:25:08 +0100

    remove travis and restructure build

--- build/06-javadoc.sh
+++ build/06-javadoc.sh
@@ -3,4 +3,15 @@ set -ue
 . build/tmp/prep.sh
 ################################################################
 echo "...make javadoc"
-makeAllJavaDocJars "${units[@]}"
+
+mkdir -p out/artifacts
+for n in "${units[@]}"; do
+   cacheFile="cache/$n/javadoc.jar"
+  targetFile="out/artifacts/$n-SNAPSHOT-javadoc.jar"
+  if [[ -f "$cacheFile" ]]; then
+    cp "$cacheFile" "$targetFile"
+  else
+    makeAllJavaDocJars "$n"
+    cp "$targetFile" "$cacheFile"
+  fi
+done
