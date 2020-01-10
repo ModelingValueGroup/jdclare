42562f42a3248abdf994bfebf1b7b261f04d1b40
Tom Brus <tombrus@gmail.com>
2020-01-10 13:58:13 +0100

    solve incompatability between javac and eclipse compiler

--- org.modelingvalue.jdclare/src/org/modelingvalue/jdclare/meta/DClass.java
+++ org.modelingvalue.jdclare/src/org/modelingvalue/jdclare/meta/DClass.java
@@ -1,41 +1,34 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// (C) Copyright 2018 Modeling Value Group B.V. (http://modelingvalue.org)                                             ~
+// (C) Copyright 2018-2019 Modeling Value Group B.V. (http://modelingvalue.org)                                        ~
 //                                                                                                                     ~
-// Licensed under the GNU Lesser General Public License v3.0 (the "License"). You may not use this file except in      ~
+// Licensed under the GNU Lesser General Public License v3.0 (the 'License'). You may not use this file except in      ~
 // compliance with the License. You may obtain a copy of the License at: https://choosealicense.com/licenses/lgpl-3.0  ~
 // Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on ~
-// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the  ~
+// an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the  ~
 // specific language governing permissions and limitations under the License.                                          ~
 //                                                                                                                     ~
+// Maintainers:                                                                                                        ~
+//     Wim Bast, Tom Brus, Ronald Krijgsheld                                                                           ~
 // Contributors:                                                                                                       ~
-//     Wim Bast, Carel Bast, Tom Brus, Arjan Kok, Ronald Krijgsheld                                                    ~
+//     Arjan Kok, Carel Bast                                                                                           ~
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 package org.modelingvalue.jdclare.meta;
 
+import org.modelingvalue.collections.*;
+import org.modelingvalue.dclare.*;
+import org.modelingvalue.jdclare.*;
+
+import java.lang.reflect.*;
+
 import static org.modelingvalue.jdclare.PropertyQualifier.*;
-
-import java.lang.reflect.Method;
-
-import org.modelingvalue.collections.Collection;
-import org.modelingvalue.collections.ContainingCollection;
-import org.modelingvalue.collections.Entry;
-import org.modelingvalue.collections.Map;
-import org.modelingvalue.collections.Set;
-import org.modelingvalue.dclare.Mutable;
-import org.modelingvalue.dclare.MutableClass;
-import org.modelingvalue.dclare.Observer;
-import org.modelingvalue.dclare.Setable;
-import org.modelingvalue.jdclare.DClare;
-import org.modelingvalue.jdclare.DObject;
-import org.modelingvalue.jdclare.Property;
 
 public interface DClass<T extends DObject> extends DStructClass<T>, MutableClass {
 
-    @SuppressWarnings({"unchecked", "rawtypes"})
+    @SuppressWarnings({"rawtypes"})
     @Property(constant)
     default Set<DRule> allRules() {
-        return allSupers().filter(DClass.class).<DRule> flatMap(s -> s.rules()).toSet();
+        return allSupers().filter(DClass.class). <DRule>flatMap(DClass::rules).toSet();
     }
 
     @Property(constant)
@@ -88,13 +81,13 @@ public interface DClass<T extends DObject> extends DStructClass<T>, MutableClass
     @Override
     @SuppressWarnings({"unchecked", "rawtypes"})
     default Collection<? extends Observer<?>> dObservers() {
-        return (Collection) allRules().map(DRule::observer);
+        return allRules().map(DRule::observer);
     }
 
     @SuppressWarnings("unchecked")
     @Override
     default Collection<? extends Setable<? extends Mutable, ?>> dSetables() {
-        return allProperties().map(DClare::getable).filter(Setable.class);
+        return allProperties().map(DClare::getable).filter(Setable.class).map(s -> (Setable <? extends Mutable, ?>) s);
     }
 
 }
